9a6720cada9cd473014801ce6413cb78bb16a008
Pardhasaradhi <mekalapardhu1@gmail.com>
2024-09-29 22:27:45 +0530

    ready to deploy

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const PORT = process.env.PORT;
 
 mongoDbConnection.connect();
 
-app.use(cors({origin:'*'}));
+app.use(cors({
+  origin: 'https://task-flow-9xhd.vercel.app', // Your frontend origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
+  credentials: true, // If you need to send cookies or authentication headers
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/auth", users);
