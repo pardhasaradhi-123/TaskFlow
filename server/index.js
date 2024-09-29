5c5d81b81eee37623a39751c75f29a3e975e1b00
Pardhasaradhi <mekalapardhu1@gmail.com>
2024-09-29 22:23:13 +0530

    final

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT;
 
 mongoDbConnection.connect();
 
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1/auth", users);
