e9d1f8b6448d6c3255deec19db582cc5ecb70115
Pardhasaradhi <mekalapardhu1@gmail.com>
2024-10-02 16:22:38 +0530

    final

--- server/index.js
+++ server/index.js
@@ -10,14 +10,17 @@ const PORT = process.env.PORT || 5000; // Fallback to port 5000 if PORT isn't de
 // Connect to MongoDB
 mongoDbConnection.connect();
 
-// CORS Middleware with specific origin
-const corsOptions = {
-  origin: process.env.APPLICATION_URL, // Allow only your client
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-};
-
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Orgin", "http://localhost:4200");
+  res.header(
+    "Access-Control-Allow-Header",
+    "Orgin,X-Request-With, Content-Type, Accept"
+  );
+  res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.header("Pragma", "no-cache");
+  res.header("Expires", 0);
+  next();
+});
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(express.json());
